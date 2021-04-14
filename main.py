43aa49758b7aa8768df17f694d0d8a47b9936057
Christian Coenen <coenen.christian@outlook.com>
2021-04-14 15:12:09 +0200

    Cleanup (#2)
    * Update Poetry configuration
    
    * Add MIT license
    
    * Fix wrong indentation
    
    * Fixes an error where uploads fail when no game_name is provided
    
    * Remove unused code
    
    * Update parameter help texts
    
    * Add -k argument (to toggle 'made for kids')
    
    * Multiple bugfixes due to adjusted YouTube upload layout
    
    * Adjust DateTime parameter to allow string inputs
    
    * Added uploads mount to docker-compose
    
    * Update .gitignore
    
    * Restrict Python version to 3.8.X
    
    Co-authored-by: Luca Schilling
    Co-authored-by: MorMundHS-MA

---
+++ main.py
@@ -0,0 +1,137 @@
+import argparse
+import logging
+from argparse import ArgumentError
+from datetime import datetime
+
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.remote.file_detector import LocalFileDetector
+
+from src.login import confirm_logged_in, login_using_cookie_file
+from src.upload import upload_file
+
+
+def main():
+    logging.getLogger().setLevel(logging.INFO)
+
+    # Setup Selenium web driver
+    parser = get_arg_parser()
+    args = parser.parse_args()
+
+    if args.browser == "docker":
+        driver = webdriver.Remote(
+            command_executor="http://127.0.0.1:4444/wd/hub",
+            desired_capabilities=DesiredCapabilities.FIREFOX,
+        )
+    elif args.browser == "firefox":
+        firefox_profile = webdriver.FirefoxProfile()
+        firefox_profile.set_preference("intl.accept_languages", "en-us")
+        firefox_profile.update_preferences()
+        driver = webdriver.Firefox(firefox_profile)
+    elif args.browser == "chrome":
+        driver = webdriver.Chrome()
+    else:
+        raise ArgumentError(message="Unknown driver.")
+
+    driver.set_window_size(1920, 1080)
+    login_using_cookie_file(driver, cookie_file=args.login_cookies)
+    driver.get("https://www.youtube.com")
+
+    assert "YouTube" in driver.title
+
+    try:
+        confirm_logged_in(driver)
+        driver.get("https://studio.youtube.com")
+        assert "Channel dashboard" in driver.title
+        driver.file_detector = LocalFileDetector()
+        upload_file(
+            driver,
+            video_path=args.video_path,
+            title=args.title,
+            thumbnail_path=args.thumbnail,
+            description=args.description,
+            game=args.game,
+            kids=args.kids,
+            upload_time=args.upload_time,
+        )
+    except:
+        driver.close()
+        raise
+
+
+def get_arg_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    today = datetime.now()
+    parser.add_argument(
+        "-B",
+        "--browser",
+        choices=["docker", "chrome", "firefox"],
+        default="docker",
+        type=str,
+        help="Select the driver/browser to use for executing the script (default: docker).",
+    )
+    parser.add_argument(
+        "-l",
+        "--login-cookies-path",
+        dest="login_cookies",
+        type=str,
+        help="A json file that contains the cookies required to sign into YouTube in the target browser.",
+        required=True,
+    )
+    parser.add_argument(
+        "video_path",
+        help="Path to the video file. When using docker, this path has to be inside the container "
+             "(default mount is /uploads/).",
+    )
+    parser.add_argument(
+        "--thumbnail-path",
+        "-T",
+        help="Path to the thumbnail file (default: None).",
+        dest="thumbnail",
+        type=str,
+        default=None,
+        required=False,
+    )
+    parser.add_argument(
+        "-t",
+        "--title",
+        help="This argument declares the title of the uploaded video.",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "-d",
+        "--description",
+        help="This argument declares the description of the uploaded video.",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "-g",
+        "--game",
+        help="This argument declares the game of the uploaded video (default: None).",
+        default=None,
+        required=False,
+    )
+    parser.add_argument(
+        "-k",
+        "--kids",
+        help="Whether the video is made for kids or not. (default: False)",
+        required=False,
+        type=bool,
+        default=False,
+    )
+    parser.add_argument(
+        "-ut",
+        "--upload_time",
+        help="This argument declares the scheduled upload time (UTC) of the uploaded video. "
+             "(Example: 2021-04-04T20:00:00)",
+        required=False,
+        type=datetime.fromisoformat,
+        default=datetime(today.year, today.month, today.day, 20, 15),
+    )
+    return parser
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/upload.py
@@ -0,0 +1,155 @@
+import logging
+import re
+from datetime import datetime
+from time import sleep
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def upload_file(
+        driver: WebDriver,
+        video_path: str,
+        title: str,
+        description: str,
+        game: str,
+        kids: bool,
+        upload_time: datetime,
+        thumbnail_path: str = None,
+):
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "ytcp-button#create-icon"))).click()
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH, '//tp-yt-paper-item[@test-id="upload-beta"]'))
+    ).click()
+    video_input = driver.find_element_by_xpath('//input[@type="file"]')
+    video_input.send_keys(video_path)
+
+    _set_basic_settings(driver, title, description, thumbnail_path)
+    _set_advanced_settings(driver, game, kids)
+    # Go to visibility settings
+    for i in range(3):
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "next-button"))).click()
+
+    _set_time(driver, upload_time)
+    _wait_for_processing(driver)
+    # Go back to endcard settings
+    driver.find_element_by_css_selector("#step-badge-1").click()
+    _set_endcard(driver)
+
+    for _ in range(2):
+        # Sometimes, the button is clickable but clicking it raises an error, so we add a "safety-sleep" here
+        sleep(5)
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "next-button"))).click()
+
+    sleep(5)
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "done-button"))).click()
+
+    # Wait for the dialog to disappear
+    sleep(5)
+    logging.info("Upload is complete")
+
+
+def _wait_for_processing(driver):
+    # Wait for processing to complete
+    progress_label: WebElement = driver.find_element_by_css_selector("span.progress-label")
+    pattern = re.compile(r"(finished processing)|(processing hd.*)|(check.*)")
+    current_progress = progress_label.get_attribute("textContent")
+    last_progress = None
+    while not pattern.match(current_progress.lower()):
+        if last_progress != current_progress:
+            logging.info(f'Current progress: {current_progress}')
+        last_progress = current_progress
+        sleep(5)
+        current_progress = progress_label.get_attribute("textContent")
+
+
+def _set_basic_settings(driver: WebDriver, title: str, description: str, thumbnail_path: str = None):
+    title_input: WebElement = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable(
+            (
+                By.XPATH,
+                '//ytcp-mention-textbox[@label="Title"]//div[@id="textbox"]',
+
+            )
+        )
+    )
+
+    # Input meta data (title, description, etc ... )
+    description_input: WebElement = driver.find_element_by_xpath(
+        '//ytcp-mention-textbox[@label="Description"]//div[@id="textbox"]'
+    )
+    thumbnail_input: WebElement = driver.find_element_by_css_selector(
+        "input#file-loader"
+    )
+
+    title_input.clear()
+    title_input.send_keys(title)
+    description_input.send_keys(description)
+    if thumbnail_path:
+        thumbnail_input.send_keys(thumbnail_path)
+
+
+def _set_advanced_settings(driver: WebDriver, game_title: str, made_for_kids: bool):
+    # Open advanced options
+    driver.find_element_by_css_selector("#toggle-button").click()
+    if game_title:
+        game_title_input: WebElement = driver.find_element_by_css_selector(
+            ".ytcp-form-gaming > "
+            "ytcp-dropdown-trigger:nth-child(1) > "
+            ":nth-child(2) > div:nth-child(3) > input:nth-child(3)"
+        )
+        game_title_input.send_keys(game_title)
+
+        # Select first item in game drop down
+        WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable(
+                (
+                    By.CSS_SELECTOR,
+                    "#text-item-2",  # The first item is an empty item
+                )
+            )
+        ).click()
+
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
+        (By.NAME, "MADE_FOR_KIDS" if made_for_kids else "NOT_MADE_FOR_KIDS")
+    )).click()
+
+
+def _set_endcard(driver: WebDriver):
+    # Add endscreen
+    driver.find_element_by_css_selector("#endscreens-button").click()
+    sleep(5)
+
+    # Select endcard type from last video or first suggestion if no prev. video
+    driver.find_element_by_css_selector("div.card:nth-child(1)").click()
+
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, "save-button"))).click()
+
+
+def _set_time(driver: WebDriver, upload_time: datetime):
+    # Start time scheduling
+    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "SCHEDULE"))).click()
+
+    # Open date_picker
+    driver.find_element_by_css_selector("#datepicker-trigger > ytcp-dropdown-trigger:nth-child(1)").click()
+
+    date_input: WebElement = driver.find_element_by_css_selector("input.tp-yt-paper-input")
+    date_input.clear()
+    # Transform date into required format: Mar 19, 2021
+    date_input.send_keys(upload_time.strftime("%b %d, %Y"))
+    date_input.send_keys(Keys.RETURN)
+
+    # Open time_picker
+    driver.find_element_by_css_selector(
+        "#time-of-day-trigger > ytcp-dropdown-trigger:nth-child(1) > div:nth-child(2)"
+    ).click()
+
+    time_list = driver.find_elements_by_css_selector("tp-yt-paper-item.tp-yt-paper-item")
+    # Transform time into required format: 8:15 PM
+    time_str = upload_time.strftime("%I:%M %p").strip("0")
+    time = [time for time in time_list[2:] if time.text == time_str][0]
+    time.click()
